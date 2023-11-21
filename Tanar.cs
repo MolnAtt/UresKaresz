8b4834fcfd447e89f05564cdac3a8ac0ca1308b0
Molnár Attila <molnar.attila@szlgbp.hu>
2023-11-21 22:38:57 +0100

    Add project files.

---
+++ Tanar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Karesz
+{
+    public partial class Form1 : Form
+    {
+        string betöltendő_pálya = "palya04.txt";
+
+        void TANÁR_ROBOTJAI()
+        {
+            new Robot("Karesz", 10, 10, 10, 10, 0, 5, 28, 0);
+
+            Betölt("palya01.txt");
+
+        }
+    }
+}
